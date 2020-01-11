2c1536b804ce28e83f0ba7edbf98eed31018a074
qurzschluss <59779747+qurzschluss@users.noreply.github.com>
2020-01-11 23:49:58 +0100

    Update groupwatercheck.py

--- groupwatercheck.py
+++ groupwatercheck.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python
 
-'''Hello Farmware
-
-A simple Farmware example that tells FarmBot to log a new message.
-'''
-
 from farmware_tools import device
 
 device.log(message='Hello Farmware!', message_type='success')
